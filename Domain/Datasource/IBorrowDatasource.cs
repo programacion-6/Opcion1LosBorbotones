dbc6bb5e393037557ec052d075891b46f61360f9
DiegoFigueroaSevillano <diego.figueroa.8032745@usalesiana.edu.bo>
2024-08-20 19:17:30 -0400

    Borrow datasource and repository interfaces

---
+++ Domain/Datasource/IBorrowDatasource.cs
@@ -0,0 +1,13 @@
+using Opcion1LosBorbotones.Domain.Entity;
+using Opcion1LosBorbotones.Domain.Services;
+
+namespace Opcion1LosBorbotones.Domain.Datasource;
+
+public interface IBorrowDatasource : ICrudOperations<Borrow>
+{
+    Task<IEnumerable<Borrow>> GetBorrowsByPatron(string patronId);
+    Task<IEnumerable<Borrow>> GetBorrowsByBook(string bookId);
+    Task<IEnumerable<Borrow>> GetBorrowsByStatus(BorrowStatus status);
+    Task<IEnumerable<Borrow>> GetBorrowsByDueDate(DateTime dueDate);
+    Task<IEnumerable<Borrow>> GetBorrowsByBorrowDate(DateTime borrowDate);
+}
