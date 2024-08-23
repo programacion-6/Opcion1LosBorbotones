1135c7c84a2360c7d298c27eeb85798251925c65
DiegoFigueroaSevillano <diego.figueroa.8032745@usalesiana.edu.bo>
2024-08-23 12:55:12 -0400

    Book Pagination

--- Domain/Repository/IBookRepository.cs
+++ Domain/Repository/IBookRepository.cs
@@ -3,9 +3,9 @@ namespace Opcion1LosBorbotones.Domain.Repository;
 
 public interface IBookRepository : ICrudOperations<Book>
 {
-    Task<IEnumerable<Book>> GetBooksByTitleAsync(string title);
-    Task<IEnumerable<Book>> GetBooksByAuthorAsync(string author);
     Task<Book?> GetBookByIsbnAsync(long isbn);
-    Task<IEnumerable<Book>> GetBooksByGenreAsync(BookGenre genre);
-    Task<IEnumerable<Book>> GetBooksByPublicationYearAsync(DateTime publicationYear);
+    Task<IEnumerable<Book>> GetBooksByTitleAsync(string title, int offset, int limit);
+    Task<IEnumerable<Book>> GetBooksByAuthorAsync(string author, int offset, int limit);
+    Task<IEnumerable<Book>> GetBooksByGenreAsync(BookGenre genre, int offset, int limit);
+    Task<IEnumerable<Book>> GetBooksByPublicationYearAsync(DateTime publicationYear, int offset, int limit);
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
-using Opcion1LosBorbotones.Presentation;
+using Opcion1LosBorbotones.Domain;
+using Opcion1LosBorbotones.Infrastructure.Datasource;
+using Opcion1LosBorbotones.Infrastructure.Repository;
 
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        MainMenu.InitializeApp();
+        var repository = new BookRepositoryImplementation(new BookDatasourceImplementation());
+        
+        Console.WriteLine("PRIMEROS 3");
+        //primeros 3
+        IEnumerable<Book> allBorrows = await repository.GetAllAsync(0,3);
+        foreach (var book in allBorrows)
+        {
+            Console.WriteLine(book.ToString());
+            
+        }
+        
+        Console.WriteLine("SIGUIENTES 3");
+        //siguientes 3
+        allBorrows = await repository.GetAllAsync(3,3);
+        foreach (var book in allBorrows)
+        {
+            Console.WriteLine(book.ToString());
+            
+        }
+        
+        Console.WriteLine("PRIMEROS 3 DE GENERO HORROR");
+        allBorrows = await repository.GetBooksByGenreAsync(BookGenre.Horror,0,3);
+        foreach (var book in allBorrows)
+        {
+            Console.WriteLine(book.ToString());
+            
+        }
     }
 }
