8f26eca180a2cea4a85143a54bc854982709d076
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-08-31 16:00:01 -0400

    refactor: pagination search interface made more abstract; unnecessary methods from specific searcher were removed

--- Domain/Services/ISearchStrategy.cs
+++ Domain/Services/ISearchStrategy.cs
@@ -2,6 +2,5 @@ namespace Opcion1LosBorbotones.Domain;
 
 public interface ISearchStrategy<T, I>
 {
-    public string GetPrompt();
     public Task<List<T>> SearchByPage(I criteria, int pageNumber, int pageSize);
 }
