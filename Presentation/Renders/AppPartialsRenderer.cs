08b782182eed4232cf9cc1f16df8cbee1782ba02
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-08-31 16:40:14 -0400

    refactor: renamed the partial renderer for the application

---
+++ Presentation/Renders/AppPartialsRenderer.cs
@@ -0,0 +1,19 @@
+using Spectre.Console;
+
+namespace Opcion1LosBorbotones.Presentation.Renders;
+
+public static class AppPartialsRenderer
+{
+    public static void RenderHeader()
+    {
+        AnsiConsole.Clear();
+        AnsiConsole.Write(
+            new FigletText("Los Borbotones Library")
+                .Centered()
+                .Color(Color.Aqua));
+
+        var panel = new Panel("[green]Samuel Escalera \n Diego Figuera \n Bianca Soliz[/]");
+        panel.Header = new PanelHeader("BY");
+        AnsiConsole.Write(panel);
+    }
+}
