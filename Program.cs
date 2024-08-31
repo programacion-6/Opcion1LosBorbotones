d86652bf99ad1fcc6b70dfb7d5c66db264da3e01
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-08-31 17:46:34 -0400

    feat: facade application implemented to hide the logic to initialize the application; use of the facade dessing pattern

--- Program.cs
+++ Program.cs
@@ -1,39 +1,11 @@
-using LibrarySystem;
-using Opcion1LosBorbotones;
-using Opcion1LosBorbotones.Domain;
-using Opcion1LosBorbotones.Domain.Data;
-using Opcion1LosBorbotones.Domain.Entity;
-using Opcion1LosBorbotones.Domain.Repository;
-using Opcion1LosBorbotones.Infrastructure.Repository;
-using Opcion1LosBorbotones.Infrastructure.Services.Borrows;
-using Opcion1LosBorbotones.Presentation;
-using Opcion1LosBorbotones.Presentation.Reports;
-
+namespace Opcion1LosBorbotones.Presentation.Executors;
 
 public class Program
 {
-    public static async Task Main(string[] args)
+    public static async Task Main(string[] _)
     {
-        var databaseConfig = new DatabaseConfig();
-        using var connection = databaseConfig.CreateConnection();
-
-        await connection.OpenAsync();
-
-        IBookRepository bookRepository = new BookRepository(databaseConfig.ConnectionString);
-        IPatronRepository patronRepository = new PatronRepository(databaseConfig.ConnectionString);
-        IBorrowRepository borrowRepository = new BorrowRepository(databaseConfig.ConnectionString);
-
-        IEntityRequester<Book> bookRequester = new BookRequesterByConsole();
-        IEntityRequester<Patron> patronRequester = new PatronRequesterByConsole();
-
-        var borrowService = new BorrowService(borrowRepository);
-
-        var bookOptions = new BookOptions(bookRepository, bookRequester);
-        var patronOptions = new PatronOptions(patronRepository, patronRequester);
-        var borrowOptions = new BorrowOptions(borrowService);
-        var reportOptions = new ReportsOptions(borrowRepository);
-
-        var app = new MainMenu(bookOptions, patronOptions, borrowOptions, reportOptions);
-        await app.InitializeApp();
+        var appFacade = new ApplicationFacade();
+        var mainExecutor = await appFacade.CreateAppAsync();
+        await mainExecutor.Execute();
     }
 }
