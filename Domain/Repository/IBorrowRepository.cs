12f936d0da7eb466ecc25b3f7e16205607a2f102
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-09-01 20:43:27 -0400

    feat: add new borrow status update using id

--- Domain/Repository/IBorrowRepository.cs
+++ Domain/Repository/IBorrowRepository.cs
@@ -9,4 +9,5 @@ public interface IBorrowRepository : IRepository<Borrow>
     Task<IEnumerable<Borrow>> GetBorrowsByStatus(BorrowStatus status, int offset, int limit);
     Task<IEnumerable<Borrow>> GetBorrowsByDueDate(DateTime dueDate, int offset, int limit);
     Task<IEnumerable<Borrow>> GetBorrowsByBorrowDate(DateTime borrowDate, int offset, int limit);
+    Task<bool> UpdateBorrowStatus(Guid borrowId, BorrowStatus newStatus);
 }
